98d4fe989387e97065450570b3e923b5b5e17162
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-03-08 16:37:53 +0700

    void input

--- 088_BubbleShort/088_BubbleShort.cpp
+++ 088_BubbleShort/088_BubbleShort.cpp
@@ -9,5 +9,18 @@ void input() {
 		cout << "Maukan banyaknya elemen pada array :";
 		cin >> n;
 		if (n <= 20)
+			break;
+		else {
+			cout << "\n Array dapat mempunyai maksimal 20 elemen. \n";
+		}
+	}
+	cout << "====================" << endl;
+	cout << "Masukan elemen array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
 	}
 }
+
