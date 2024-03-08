70058bb36734f17108f07fa499a450a5a34f1e68
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-03-08 16:28:31 +0700

    Add project files.

---
+++ 088_BubbleShort/088_BubbleShort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input() {
+	while (true) {
+		cout << "Maukan banyaknya elemen pada array :";
+		cin >> n;
+		if (n <= 20)
+	}
+}
